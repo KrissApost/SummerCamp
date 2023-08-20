7a3b830f10cf97a48a8e39c723cbdac61628e738
KrissApost <krissapostolov@gmail.com>
2023-08-20 09:50:06 +0300

    ukazateli

---
+++ ukazateli.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl '\n';
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int a = 5;
+    int *pa;
+    pa = &a;
+    cout << pa << endl;
+
+    return 0;
+}
